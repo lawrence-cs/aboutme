0091a675a21ec687b8bfec1e17cd2bc7518f6a23
Lawrence <lawrencesperez@gmail.com>
2020-05-09 15:07:11 +1000

    nav

--- src/components/navigation_bar.component.js
+++ src/components/navigation_bar.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Navbar from 'react-bootstrap/Navbar'
+import { Navbar, Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 
 export default class Navigation_Bar extends Component {
