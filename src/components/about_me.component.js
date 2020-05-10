c7daa30e53b40718a493bd7c91f07a080e735ff6
Lawrence <lawrencesperez@gmail.com>
2020-05-10 19:49:53 +1000

    footer

--- src/components/about_me.component.js
+++ src/components/about_me.component.js
@@ -4,7 +4,13 @@ export default class About_Me extends Component {
   render() {
     return (
     	<div className="container">
-      		<p>about me</p>
+      		<h1>Career Objective</h1>
+			<p>
+				I am looking for a suitable position with a company where outstanding performance is recognised and where I can work on a variety of projects.
+			</p>
+			<p>
+				I am problem solver with a passion for technology, and I would like to be part of company that indulges professional growth which provides challenging and rewarding career while allowing me to utilise my skills and knowledge.
+			</p>
     	</div>
     )
   }
