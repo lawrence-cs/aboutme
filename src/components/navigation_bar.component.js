832368800bb091e5cd084c1c35819cb448ec7c6b
Lawrence <lawrencesperez@gmail.com>
2020-05-10 20:19:07 +1000

    about me

--- src/components/navigation_bar.component.js
+++ src/components/navigation_bar.component.js
@@ -12,19 +12,19 @@ export default class Navigation_Bar extends Component {
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav>
             <li className="navbar-item">
-              <Link activeStyle={{ color: '#ddd'; border-bottom: '2px solid #ddd' }} to="/about_me" className="nav-link">About Me</Link>
+              <Link activeStyle={{ color: '#ddd'; border-bottom: '2px solid #ddd'; }} to="/about_me" className="nav-link">About Me</Link>
             </li>
             <li className="navbar-item">
-              <Link activeStyle={{ color: '#ddd'; }} to="/work_experience" className="nav-link">Work Experience</Link>
+              <Link activeStyle={{ color: '#ddd'; border-bottom: '2px solid #ddd'; }} to="/work_experience" className="nav-link">Work Experience</Link>
             </li>
             <li className="navbar-item">
-              <Link activeStyle={{ color: '#ddd'; }} to="/notable_works" className="nav-link">Notable Works</Link>
+              <Link activeStyle={{ color: '#ddd'; border-bottom: '2px solid #ddd'; }} to="/notable_works" className="nav-link">Notable Works</Link>
             </li>
             <li className="navbar-item">
-              <Link activeStyle={{ color: '#ddd'; }} to="/education" className="nav-link">Education</Link>
+              <Link activeStyle={{ color: '#ddd'; border-bottom: '2px solid #ddd'; }} to="/education" className="nav-link">Education</Link>
             </li>
             <li className="navbar-item">
-              <Link activeStyle={{ color: '#ddd'; }} to="/skills" className="nav-link">Skills</Link>
+              <Link activeStyle={{ color: '#ddd'; border-bottom: '2px solid #ddd'; }} to="/skills" className="nav-link">Skills</Link>
             </li>
           </Nav>
         </Navbar.Collapse>
