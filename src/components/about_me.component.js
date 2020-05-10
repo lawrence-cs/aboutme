c15b039dab237ba9bc9b458c29805d1e484cc073
Lawrence <lawrencesperez@gmail.com>
2020-05-10 21:28:13 +1000

    about me

--- src/components/about_me.component.js
+++ src/components/about_me.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 export default class About_Me extends Component {
   render() {
