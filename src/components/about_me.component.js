91acc30b157eebd65ede5a8bc16c8e860d5ccca7
Lawrence <lawrencesperez@gmail.com>
2020-05-10 20:03:38 +1000

    about me

--- src/components/about_me.component.js
+++ src/components/about_me.component.js
@@ -4,13 +4,18 @@ export default class About_Me extends Component {
   render() {
     return (
     	<div className="container">
-      		<h1 style={{ textTransform: 'uppercase'}}>Career Objective</h1>
-			<p>
-				I am looking for a suitable position with a company where outstanding performance is recognised and where I can work on a variety of projects.
-			</p>
-			<p>
-				I am problem solver with a passion for technology, and I would like to be part of company that indulges professional growth which provides challenging and rewarding career while allowing me to utilise my skills and knowledge.
-			</p>
+    		<div className="col-md-4">
+    			
+    		</div>
+    		<div className="col-md-8">
+	      		<h1 style={{ textTransform: 'uppercase'}}>Career Objective</h1>
+				<p>
+					I am looking for a suitable position with a company where outstanding performance is recognised and where I can work on a variety of projects.
+				</p>
+				<p>
+					I am problem solver with a passion for technology, and I would like to be part of company that indulges professional growth which provides challenging and rewarding career while allowing me to utilise my skills and knowledge.
+				</p>
+			</div>
     	</div>
     )
   }
